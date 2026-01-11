7b0ce9f905bb168c24027bd425c17d7d3823dce3
sachina0000 <sachinagrawal.tech@gmail.com>
2026-01-11 18:35:47 +0530

    new line is added

--- Demo1/src/com/thrivesup/ctl/Demo.java
+++ Demo1/src/com/thrivesup/ctl/Demo.java
@@ -5,6 +5,8 @@ public class Demo {
 	public static void main(String[] args) {
 		
 		System.out.println("this is Demo class");
+		
+		System.out.println("the new line is added in Demo class");
 	}
 
 }
---
+++ Demo1/src/com/thrivesup/ctl/Test.java
@@ -0,0 +1,10 @@
+package com.thrivesup.ctl;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("this is Test class");
+	}
+
+}
